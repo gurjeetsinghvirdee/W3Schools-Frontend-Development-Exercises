a12fbd7fc29b1776ef1b9e8cd835796c09a6fa72
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2024-02-06 00:46:41 +0530

    typo mistake fixed

--- JavaScript/Strings/Examples/String-3.js
+++ JavaScript/Strings/Examples/String-3.js
@@ -1,3 +1,3 @@
 let age = 23;
 let sentence = `My name is ${name} and I am ${age} years old.`;
-console.log(sentence);   // Outputs: My name is John Doe and I am 25 years old.
+console.log(sentence);   // Outputs: My name is John Doe and I am 23 years old.
